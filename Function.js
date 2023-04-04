4ec68ed2f13156008ee975a089af1cfeb13d59d8
Muhammad-Rebaal <122050424+Muhammad-Rebaal@users.noreply.github.com>
2023-04-04 13:33:52 +0500

    Add files via upload

---
+++ Function.js
@@ -0,0 +1,14 @@
+                                // Functions
+// function sum(a,b){
+//     return a+b
+// }
+// b = sum(10,39)
+// console.log(b)
+// const c = (a,b)=>a+b
+
+// console.log(c(10,39))
+
+// const d = function (a,b){
+//      return a+b
+//      }
+// console.log(d(39,10))
